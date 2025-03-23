b245dd4aacee49e587ff30779d6b3215aade1daf
Mujahid alabdullah <mujahidalabdullah052@gmail.com>
2025-03-23 22:25:01 +0300

    ARActivity works but 3d model didnt appear

--- app/src/main/java/com/achelmas/numart/ARActivity.kt
+++ app/src/main/java/com/achelmas/numart/ARActivity.kt
@@ -1,29 +1,31 @@
 package com.achelmas.numart
 
-import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import io.github.sceneview.ar.ArSceneView
 import io.github.sceneview.ar.node.ArModelNode
 import io.github.sceneview.math.Position
 
-class ARActivity : AppCompatActivity() {
+class PuzzleGameActivity : AppCompatActivity() {
 
-    private lateinit var arSceneView: ArSceneView
     private lateinit var sceneView: ArSceneView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_ar)
-        arSceneView = findViewById(R.id.arSceneVie)
-        val modelUri = Uri.parse("models/puzzle_numbers.glb")
-        val numberNode = ArModelNode().apply {
-            loadModelGlbAsync( glbFileLocation = modelUri.toString()) {
-                scale = Position(0.005f, 0.005f, 0.005f) // Modelin boyutunu ayarla
-                this.position =   Position(-1.0f, 0.5f, -1.5f) // Modelin sahnedeki pozisyonunu ayarla
+        setContentView(R.layout.activity_ar) // Ensure you create this XML file
+
+        sceneView = findViewById(R.id.arSceneView)
+
+        loadPuzzleModel()
+    }
+
+    private fun loadPuzzleModel() {
+        val puzzleNode = ArModelNode().apply {
+            loadModelGlbAsync("models/number1.glb") {
+                scale = Position(1f, 1f, 1f) // Adjust size
+                this.position = Position(-1.0f, 0.5f, -1.5f) // Adjust position
             }
-
         }
-        arSceneView.addChild(numberNode)
-
+        sceneView.addChild(puzzleNode)
     }
 }
